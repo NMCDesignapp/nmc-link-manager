b52dcba0c10f7592a98dbc4a9de68368f261da91
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-08-07 19:20:54 +0700

    feat: load new KPI cyber room background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -51,6 +51,7 @@ export default function RootLayout({
       <head>
         <link rel="apple-touch-icon" href="/icon/nc-link-180.png" />
         <link rel="stylesheet" href="/kpi-ui-overrides.css" />
+        <link rel="stylesheet" href="/kpi-cyber-room-v4.css" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
