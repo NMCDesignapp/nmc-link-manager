9a6dc4bc13fb13dd3868f46112073615902ad142
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-08-31 17:40:24 +0700

    feat(android): wire KPI production URL

---
+++ android/kpiapp/src/main/java/com/nmclink/kpi/MainActivity.java
@@ -0,0 +1,10 @@
+package com.nmclink.kpi;
+
+import com.nmclink.shell.BaseWebViewActivity;
+
+public final class MainActivity extends BaseWebViewActivity {
+    @Override
+    protected String startUrl() {
+        return "https://angiang2026.vercel.app/";
+    }
+}
