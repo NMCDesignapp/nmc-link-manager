c88a9451953af54aa2a50b9a3b3310ed09c80214
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-08-31 17:40:18 +0700

    feat(android): wire Main App production URL

---
+++ android/mainapp/src/main/java/com/nmclink/main/MainActivity.java
@@ -0,0 +1,10 @@
+package com.nmclink.main;
+
+import com.nmclink.shell.BaseWebViewActivity;
+
+public final class MainActivity extends BaseWebViewActivity {
+    @Override
+    protected String startUrl() {
+        return "https://nc-link.vercel.app/";
+    }
+}
