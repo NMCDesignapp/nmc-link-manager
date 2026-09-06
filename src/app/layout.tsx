30d1e5e6b740aeedd9d420bfef2027ddaf4c2cab
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-09-06 07:34:35 +0700

    chore(kpi): bust detail theme cache

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,7 +85,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="/kpi-scroll-stability-v2.css?v=20260904-1" />
         <link rel="stylesheet" href="/kpi-embedded-compact-v2.css?v=20260830-1" />
         <link rel="stylesheet" href="/kpi-loader-scope-v14.css?v=20260904-1" />
-        <link rel="stylesheet" href="/kpi-detail-metal-v15.css?v=20260904-1" />
+        <link rel="stylesheet" href="/kpi-detail-metal-v15.css?v=20260906-1" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
