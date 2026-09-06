28de55186c1ebc411b1036dd0a4b8966fc71c773
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-09-07 02:43:52 +0700

    Load KPI layout polish in Main App

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -87,6 +87,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="/kpi-loader-scope-v14.css?v=20260904-1" />
         <link rel="stylesheet" href="/kpi-detail-metal-v15.css?v=20260906-1" />
         <link rel="stylesheet" href="/kpi-calendar-navy-light-v17.css?v=20260906-1" />
+        <link rel="stylesheet" href="/kpi-layout-polish-v18.css?v=20260907-1" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
