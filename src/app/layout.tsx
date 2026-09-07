bf2a310b13900c2f8889f6fea00df0ff59f1676f
NGUYEN MINH CHAU <nmchau022023@gmail.com>
2026-09-07 09:21:57 +0700

    chore(kpi): bust title polish cache in main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -88,7 +88,7 @@ export default function RootLayout({
         <link rel="stylesheet" href="/kpi-detail-metal-v15.css?v=20260906-1" />
         <link rel="stylesheet" href="/kpi-calendar-navy-light-v17.css?v=20260906-1" />
         <link rel="stylesheet" href="/kpi-layout-polish-v18.css?v=20260907-1" />
-        <link rel="stylesheet" href="/kpi-detail-align-v19.css?v=20260907-1" />
+        <link rel="stylesheet" href="/kpi-detail-align-v19.css?v=20260907-2" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
